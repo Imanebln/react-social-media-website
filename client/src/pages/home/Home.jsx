a83017ddd375cd8f1a84a57588b0a443f139ceb7
Imanebln <boulouane.imane@gmail.com>
2023-02-21 20:43:46 +0100

    added lazy load feed component

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
 import Rightbar from "../../components/rightbar/Rightbar";
-import Feed from "../../components/feed/Feed";
+// import Feed from "../../components/feed/Feed";
 import "./home.css";
+import { Suspense, lazy } from "react";
+const Feed = lazy(() => import("../../components/feed/Feed"));
 
 function Home() {
   return (
     <>
       <Topbar />
       <div className="homeContainer">
-        <Sidebar />
-        <Feed />
+        <Sidebar fallback={<div>Loading...</div>} />
+        <Suspense>
+          <Feed />
+        </Suspense>
+
         <Rightbar />
       </div>
     </>
